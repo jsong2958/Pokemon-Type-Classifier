00be9715ae7730cb0f9baa151e355c013f2a9028
jsong2958 <josephsong2958@gmail.com>
2025-05-09 05:12:46 -0400

    fixed padding

--- frontend/src/components/Prediction.jsx
+++ frontend/src/components/Prediction.jsx
@@ -1,4 +1,5 @@
 import Type from "./Type";
+import './Prediction.css'
 
 function Prediction( {src, showImage, types} ) {
     
@@ -7,15 +8,15 @@ function Prediction( {src, showImage, types} ) {
     }
 
     return (
-        <div>
-          <img src={src} width="100px"></img>
-          <h3>Predicted types: </h3>
-          <ul>
-            {types.map((type, index) => (
-                <Type type={type} key={index}/>
-            ))}
-          </ul>
-        </div>
+      <div className="pred-container">
+        <img src={src} width="300px"></img>
+        <h3>Predicted types: </h3>
+        <ul>
+        {types.map((type, index) => (
+          <Type type={type} key={index}/>
+        ))}
+        </ul>
+      </div>
     );
 
 }
